3d66d3bcd48093e7c41f84a8414c5918a29d7122
Name <empty>
2022-07-27 07:39:20 +0300

    Добавил в _Layout.cshtml вызов действия, независимого от основного контроллера.

--- NotionV3/Components/GetCategoriesViewComponent.cs
+++ NotionV3/Components/GetCategoriesViewComponent.cs
@@ -1,22 +1,28 @@
 ﻿namespace NotionV3.Controllers {
+    using DateBaseServices;
     using DateBaseServices.Models;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore.Metadata.Internal;
     using System.Collections.Generic;
     using System.Threading.Tasks;
 
-    public class GetMenuList : ViewComponent {
-        
+    public class GetCategoriesViewComponent : ViewComponent
+    {
+        GetCategoriesViewComponent(DataContext db)
+        {
+            
+        }
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var list = new List<Category>
             {
-                new Category{ CategoryId = 1, Title = "first cat"},
-                new Category{ CategoryId = 2, Title = "Sec cat"},
-            };
+                new Category{CategoryId = 1, Title = "First"},
+                new Category{CategoryId = 2, Title = "Secound"},
 
+            };
             return View(list);
         }
-
     }
+
 }
---
+++ TestProject1/SecurityTests.cs
@@ -0,0 +1,31 @@
+﻿namespace TestProject1 
+{
+    using DateBaseServices;
+    using DateBaseServices.Models;
+    using NUnit.Framework;
+    using SecurityService.Service;
+
+    public class SecurityTests {
+        private SecurityService _service = new();
+        [SetUp] public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void CreateToken()
+        {
+            var token = _service.GenerateToken(1);
+            
+        }
+
+        [Test] public void ValidateToken()
+        {
+            CreateToken();
+            var token = _service.GenerateToken(1);
+            var validate = _service.ValidateCurrentToken(token);
+
+        }
+
+    }
+}
